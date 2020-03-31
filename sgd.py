c16fc962a6af84c5f64d70da11ce4a0a9ac2cfdc
trick <xiehouta@qq.com>
2020-03-31 09:35:34 +0800

    Add files via upload

--- sgd.py
+++ sgd.py
@@ -1,27 +1,36 @@
 from math import exp
 import random
+from collections.__main__ import Point
 
 # TODO: Calculate logistic
 def logistic(x):
-    return 1.
+    return 1./(1.+exp(-x))
 
 # TODO: Calculate dot product of two lists
 def dot(x, y):
     s = 0
+    for i,j in zip(x,y):s+=i*j
     return s
 
 # TODO: Calculate prediction based on model
 def predict(model, point):
-    return 0
+    return logistic(dot(model,point['features']))
 
 # TODO: Calculate accuracy of predictions on data
 def accuracy(data, predictions):
     correct = 0
+    for i in range(len(data)):
+        if data[i]['label']==round(predictions[i]):
+            correct+=1.    
     return float(correct)/len(data)
 
 # TODO: Update model using learning rate and L2 regularization
 def update(model, point, delta, rate, lam):
-    pass
+    loss=(logistic(dot(model,point['features']))-point['label'])**2+lam*dot(model,model)
+    print(loss)
+    if loss>0:
+        for i in range(len(model)):
+            model[i]-=rate*delta[i] 
 
 def initialize_model(k):
     return [random.gauss(0, 1) for x in range(k)]
@@ -29,6 +38,20 @@ def initialize_model(k):
 # TODO: Train model using training data
 def train(data, epochs, rate, lam):
     model = initialize_model(len(data[0]['features']))
+    def delta(point):
+        return [2*logistic(x)-2*logistic(x)**2 for x in point['features']]
+    d0=[0]*len(model)
+    for i in range(len(data)):
+        for j,k in zip(d0,delta(data[i])):
+            j+=k
+    for i in d0:i/=len(data)
+    def deltar(model,lam):
+        return [2*lam*x for x in model]
+    for _ in range(epochs):
+        for i in range(len(data)):
+            d1=[x+y for x,y in zip(d0,deltar(model,lam)) ]
+            update(model,data[i], d1,rate,lam)  
+    print(model)
     return model
         
 def extract_features(raw):
@@ -49,5 +72,5 @@ def extract_features(raw):
 
 # TODO: Tune your parameters for final submission
 def submission(data):
-    return train(data, 1, .01, 0)
+    return train(data, 1, .03, 0.2)
     
